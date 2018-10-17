8c56ee2397efd2c10be1e72e2ac740b16a2d29ee
namgillee <42762623+namgillee@users.noreply.github.com>
2018-10-17 17:28:08 +0900

    Update myFDRTOOL.R

--- functions/myFDRTOOL.R
+++ functions/myFDRTOOL.R
@@ -2,10 +2,26 @@ myFDRTOOL <- function (x, statistic = c("normal", "correlation", "pvalue"),
               plot = TRUE, color.figure = TRUE, verbose = TRUE,  
               cutoff.method = c("fndr", "pct0", "locfdr"), pct0 = 0.75) 
 {
-  ##
-  ## This code is a copy of fdrtool() from 'fdrtool' package (Strimmer, 2008)
-  ## and a modification was made to the plotting part
-  ##
+  ### This code is a copy of the function fdrtool() from the R package 
+  ### 'fdrtool' (Strimmer, 2008). A modification was made to the plotting part. 
+  ###
+  ### Copyright 2007 Korbinian Strimmer
+  ###
+  ### This file is part of the `fdrtool' library for R and related languages.
+  ### It is made available under the terms of the GNU General Public
+  ### License, version 3, or at your option, any later version,
+  ### incorporated herein by reference.
+  ### 
+  ### This program is distributed in the hope that it will be
+  ### useful, but WITHOUT ANY WARRANTY; without even the implied
+  ### warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  ### PURPOSE.  See the GNU General Public License for more
+  ### details.
+  ### 
+  ### You should have received a copy of the GNU General Public
+  ### License along with this program; if not, write to the Free
+  ### Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+  ### MA 02111-1307, USA
   
   
   if (file.exists('nullmodel.R')) 
